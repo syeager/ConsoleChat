bde3de7d78ee5d351e357d3ca50987ef98bbcd52
Steve <steve.f.yeager@gmail.com>
2020-08-28 16:33:55 -0700

    Add CommandLineParser lib

--- ConsoleChatSource/ConsoleChat/Program.cs
+++ ConsoleChatSource/ConsoleChat/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using CommandLine;
 
-namespace ConsoleChat
+namespace QuickStart
 {
     class Program
     {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    if(o.Verbose)
+                    {
+                        Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example!");
+                    }
+                });
         }
     }
 }
